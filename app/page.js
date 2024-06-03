0b2b0502298604700897ab88c23ce76503ae285a
kenzosashida090 <kenzosashida1@gmail.com>
2024-06-03 13:53:39 -0600

    ADD:Home Page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
+import bg from "@/public/bg.png"
 
 export default function Home() {
   return (
-    <div>
-      <h1>The Next Hotel. Welcome to your home!.</h1>
-      <Link href="/cabins">Explore cabins</Link>  {/*Work as a SPA and no hard reload of the entire page and avoid downloading all the bundle again */}
-    </div>
+    <main className="mt-24">
+      <Image src={bg}alt="Mountains and forests with two cabins" fill className="object-cover object-top" placeholder="blur" quality={80} />
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
