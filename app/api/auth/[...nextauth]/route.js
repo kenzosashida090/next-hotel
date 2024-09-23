346283d40675b0dc4d20e61b531dcfb73ef5744b
kenzosashida090 <kenzosashida1@gmail.com>
2024-09-23 12:16:45 -0600

    [new] AuthNext

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,3 @@
+//[...nextauth] handle whatever the route name is
+//WILL ACCESS TO THE GET POST OF THE AUTH FILE THAT WE CREATE
+export {GET, POST} from "@/app/_lib/auth" //export the GET POST THE SAME AS THE CABINS
