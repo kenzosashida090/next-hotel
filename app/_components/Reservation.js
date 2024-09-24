37f6c8f3948ecfa04d981c1f24e243a8cbc7334c
kenzosashida090 <kenzosashida1@gmail.com>
2024-09-23 18:21:21 -0600

    [NEW] session

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,16 +1,22 @@
+import { auth } from "../_lib/auth"
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service"
 import DateSelector from "./DateSelector"
+import LoginMessage from "./LoginMessage"
 import { ReservationProvider } from "./ReservationContext"
 import ReservationForm from "./ReservationForm"
 
 
  async function Reservation({cabin}) {
     const [settings,bookedDates] = await Promise.all([ getSettings(), getBookedDatesByCabinId(cabin.id)  ])
-    
+    const session = await auth()
     return (
         <>
-            <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
-            <ReservationForm  cabin={cabin}/>
+           <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
+           { session?.user ?
+              <ReservationForm  cabin={cabin} user={session.user}/>
+             :
+             <LoginMessage/>
+            }
         </>
 
     )
--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -2,7 +2,7 @@
 
 import { useReservation } from "./ReservationContext";
 
-function ReservationForm() {
+function ReservationForm({cabin,user}) {
   // CHANGE
   const maxCapacity = 23;
   const {range} = useReservation()
@@ -10,8 +10,7 @@ function ReservationForm() {
     <div className='scale-[1.01]  mr-1.6'>
       <div className='bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center'>
         <p>Logged in as</p>
-
-        {/* <div className='flex gap-4 items-center'>
+        <div className='flex gap-4 items-center'>
           <img
             // Important to display google profile images
             referrerPolicy='no-referrer'
@@ -20,9 +19,8 @@ function ReservationForm() {
             alt={user.name}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
-            <p>{String(range)}</p>
       <form className='bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col'>
         <div className='space-y-2'>
           <label htmlFor='numGuests'>How many guests?</label>
