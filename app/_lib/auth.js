830567b0ba0f88ea2caa2a863cdb91abfce49879
kenzosashida090 <kenzosashida1@gmail.com>
2024-09-23 18:41:05 -0600

    [NEW] Next Middlewares

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -8,7 +8,16 @@ const authConfig = {
             clientId: process.env.AUTH_GOOGLE_ID,
             clientSecret: process.env.AUTH_GOOGLE_SECRET
         })
-    ]
+    ],
+    callbacks: {
+        authorized({auth, request}){
+            //authorized needs to return true/false 
+            return !!auth?.user; //this is a way to transform to a boolean
+        }
+    },
+    pages:{
+        signIn:"/login"
+    }
 }
 
-export const {auth, handlers:{GET,POST}}=NextAuth(authConfig)
+export const {auth,signIn,signOut, handlers:{GET,POST}}=NextAuth(authConfig)
