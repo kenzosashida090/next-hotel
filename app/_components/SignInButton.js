c28bbe5958e635d5acceaebf5205cba437bd2ef1
kenzosashida090 <kenzosashida1@gmail.com>
2024-09-23 18:51:44 -0600

    [NEW] Sign In functionality

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,14 +1,22 @@
+import { SignInAction } from "../_lib/action";
+
 function SignInButton() {
+
+  //this is a server component so we cant set the onClick 
+  //instead we call a "server action" that allows from the browser interactivite with the server 
+
   return (
-    <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
-      <img
-        src='https://authjs.dev/img/providers/google.svg'
-        alt='Google logo'
-        height='24'
-        width='24'
-      />
-      <span>Continue with Google</span>
-    </button>
+    <form action={SignInAction}>
+      <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
+        <img
+          src='https://authjs.dev/img/providers/google.svg'
+          alt='Google logo'
+          height='24'
+          width='24'
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
--- app/account/page.js
+++ app/account/page.js
@@ -1,10 +1,14 @@
+import { auth } from "../_lib/auth"
+
 export const metadata = {
     title:"Guest area"
 }
-function page() {
+async function page() {
+    const session = await auth()
+    const firstName = session.user.name.split(" ")[0]
     return (
         <div>
-            <h2 className="font-semibold text-accent-400 mb-7 text-2xl">Welcome, Kenzo</h2>
+            <h2 className="font-semibold text-accent-400 mb-7 text-2xl">Welcome, {firstName}</h2>
         </div>
     )
 }
